f97d9c7e3718e00531c481b5f9a1c1519e6eda42
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-10-20 10:49:30 -0300

    fix: pattern of validDate

--- src/schemas/payments-schema.ts
+++ src/schemas/payments-schema.ts
@@ -7,11 +7,11 @@ export const creditCardSchema = Joi.object<CreditCard>({
     .required()
     .label('Has to be exactly 16 numbers'),
   name: Joi.string()
-    .pattern(/^[a-zA-Z]{4,30}$/)
+    .pattern(/^[a-zA-Z" "]{4,30}$/)
     .required()
     .label('Insert your first name and choose one surname'),
   validDate: Joi.string()
-    .pattern(/^[0-9]{2}\/[0-9]{2}$/)
+    .pattern(/^(0[1-9]|10|11|12)\/[0-9]{2}$/)
     .required()
     .label('Has to be in this format mm/yy, mm -> month, yy -> year'),
   cvc: Joi.string()
